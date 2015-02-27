7ff0a06b56bd9bbf4ec75cf1979e8bf854825c35
Sami M <sjmirimiri@email.wm.edu>
2015-02-26 22:27:01 -0500

    restaurants#create

--- spec/requests/restaurant_spec.rb
+++ spec/requests/restaurant_spec.rb
@@ -16,4 +16,29 @@ describe RestaurantsController  do
 			it { expect(subject).to redirect_to(:root) }
 		end
 	end
+
+	context "GET /restaurants/new" do
+		subject {get "/restaurants/new"}
+
+		it "renders new" do
+			expect(subject).to render_template(:new)
+		end
+	end
+
+	context "POST /restaurants" do
+    	context "complete params" do
+      		restaurant = {restaurant: {name: "mcrails"}}
+      		subject{ post "/restaurants", restaurant }
+      		it "redirects to show" do
+        		expect(subject).to redirect_to restaurant_path(id:1)
+      		end
+    	end
+
+    	context "incomplete params" do
+      		subject{ post "/restaurants", {}}
+      		it "redirects to new" do
+        		expect(subject).to redirect_to(new_restaurant_path)
+      		end
+    	end
+	end
 end
