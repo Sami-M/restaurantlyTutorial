acae1417c3025f038b49d6baabcd9a632f2da47a
Sami M <sjmirimiri@email.wm.edu>
2015-02-27 00:26:21 -0500

    edit integration tests

--- spec/features/restaurants_spec.rb
+++ spec/features/restaurants_spec.rb
@@ -40,3 +40,32 @@ describe "destroy links work" do
     end
   end
 end
+
+describe "edit links work" do
+  context "displays ", :driver => :selenium do
+    it "Restaurantly Spots!" do
+      visit '/restaurants/new'
+      fill_in 'restaurant_name', with: "mc ruby"
+      click_button 'submit!'
+      visit '/'
+      click_link 'edit'
+      fill_in 'restaurant_name', with: "mc rails"
+      click_button 'update'
+      expect(page).to have_content 'mc rails'
+    end
+  end
+end
+
+describe "destroy links work" do
+  context "displays ", :driver => :selenium do
+    it "Restaurantly Spots!" do
+      visit '/restaurants/new'
+      fill_in 'restaurant_name', with: "mc ruby"
+      click_button 'submit!'
+      expect(page).to have_content 'mc ruby'
+      visit '/'
+      click_link 'destroy'
+      expect(page).to have_no_content 'mc ruby'
+    end
+  end
+end
